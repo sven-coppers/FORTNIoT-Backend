80786ed4cb60c32f7e0b7623807f47d81cf9baed
Sven Coppers <sven.coppers@uhasselt.be>
2020-02-19 13:56:07 +0100

    Rewritten predictionEngine to work timebased instead of change based

--- src/main/java/sven/phd/iot/predictions/PredictionEngine.java
+++ src/main/java/sven/phd/iot/predictions/PredictionEngine.java
@@ -16,7 +16,7 @@ public class PredictionEngine {
     private Future future;
     private Boolean predicting;
     private final long tickRate = 5; // minutes
-    private final long predictionWindow = 2 * 24 * 60; // minutes
+    private final long predictionWindow = 2 * 24 * 60; // in minutes
 
     public PredictionEngine(RulesManager rulesManager, HassioDeviceManager hassioDeviceManager) {
         this.rulesManager = rulesManager;
@@ -61,28 +61,39 @@ public class PredictionEngine {
         queue.addAll(hassioDeviceManager.predictFutureStates());
         queue.addAll(simulatedStates);
         queue.addAll(stateScheduler.getScheduledStates());
-        Date lastFrameDate = null;
 
-        while(!queue.isEmpty()) {
-            // Add intermediate ticks if needed
-            if(this.isPredicting()) {
-                HassioState potentialNewState = queue.peek();
+        Date lastFrameDate = new Date(); // Prediction start
+        Date predictionEnd = new Date(new Date().getTime() + predictionWindow * 60l * 1000l); // Convert prediction window from minutes to milliseconds
 
-                if(lastFrameDate != null) {
-                    Date nextFrameDate = new Date(lastFrameDate.getTime() + (tickRate * 60l * 1000l));
+        while(lastFrameDate.getTime() < predictionEnd.getTime()) {
+            Date nextTickDate = new Date(lastFrameDate.getTime() + (tickRate * 60l * 1000l)); // Convert tickrate from minutes to milliseconds
 
-                    if(nextFrameDate.getTime() < potentialNewState.last_changed.getTime()) {
-                        queue.addAll(hassioDeviceManager.adaptStateToContext(nextFrameDate, lastStates));
-                        continue; // Skip this frame
-                    }
-                }
+            // If there is an element in the queue that will happen before the tick
+            if(!queue.isEmpty() && queue.peek().last_changed.getTime() < nextTickDate.getTime()) {
+                nextTickDate = queue.peek().last_changed;
             }
 
-            HassioState newState = queue.poll();
+            this.tick(nextTickDate, lastStates, queue, future, simulatedRulesEnabled);
+            lastFrameDate = nextTickDate;
+        }
+
+        return future;
+    }
+
+    private void tick(Date newDate, HashMap<String, HassioState> lastStates, PriorityQueue<HassioState> globalQueue, Future future, HashMap<String, Boolean> simulatedRulesEnabled) {
+        System.out.println(newDate);
+
+        // Let the devices predict their state, based on the future context
+        if(this.isPredicting()) {
+            globalQueue.addAll(hassioDeviceManager.adaptStateToContext(newDate, lastStates));
+        }
+
+        // Remove every State from the global queue that has occurred before the new Date
+        while(!globalQueue.isEmpty() && globalQueue.peek().last_changed.getTime() <= newDate.getTime()) {
+            HassioState newState = globalQueue.poll();
 
             // Log the predicted state in the device
             future.addFutureState(newState);
-            lastFrameDate = newState.last_changed;
 
             // Only when additional predictions are enabled
             if(this.isPredicting()) {
@@ -108,12 +119,10 @@ public class PredictionEngine {
                     future.addHassioRuleExecutionEventPrediction(triggerEvent);
 
                     // Add the actions to the prediction QUEUEs
-                    queue.addAll(resultingActions);
+                    globalQueue.addAll(resultingActions);
                 }
             }
         }
-
-        return future;
     }
 
    /* private void removePredictionsFromQueue(PriorityQueue<HassioState> queue, String deviceID) {
